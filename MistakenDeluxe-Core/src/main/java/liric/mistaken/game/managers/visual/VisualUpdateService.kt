4eec13e6d175a0090c7769b93287d1aa7d8daaa6
Mistaken <superzxd276@gmail.com>
2026-09-03 21:19:32 -0600

    Fix Folia scheduler support in VisualUpdateService

--- MistakenDeluxe-Core/src/main/java/liric/mistaken/game/managers/visual/VisualUpdateService.kt
+++ MistakenDeluxe-Core/src/main/java/liric/mistaken/game/managers/visual/VisualUpdateService.kt
@@ -2,13 +2,15 @@ package liric.mistaken.game.managers.visual
 
 import liric.mistaken.Mistaken
 import org.bukkit.Bukkit
-import org.bukkit.scheduler.BukkitTask
 import liric.mistaken.utils.scoreboard.ScoreboardManager as PumpkingScoreboardManager
+
+import io.papermc.paper.threadedregions.scheduler.ScheduledTask
+import java.util.concurrent.TimeUnit
 
 class VisualUpdateService(private val plugin: Mistaken) {
 
-    private var syncTask: BukkitTask? = null
-    private var asyncTask: BukkitTask? = null
+    private var syncTask: ScheduledTask? = null
+    private var asyncTask: ScheduledTask? = null
     
     private var syncTicks = 0L
     private var asyncTicks = 0L
@@ -17,16 +19,16 @@ class VisualUpdateService(private val plugin: Mistaken) {
         
         
         val scoreboardInterval = if (PumpkingScoreboardManager.supportsAnimations()) 2L else 10L
-        asyncTask = plugin.server.scheduler.runTaskTimerAsynchronously(plugin, Runnable {
+        asyncTask = plugin.server.asyncScheduler.runAtFixedRate(plugin, { _ ->
             asyncTicks += scoreboardInterval
             for (player in Bukkit.getOnlinePlayers()) {
                 plugin.scoreboardManager.updatePlayer(player)
             }
-        }, 20L, scoreboardInterval)
+        }, 1000L, scoreboardInterval * 50L, TimeUnit.MILLISECONDS)
 
         
         
-        syncTask = plugin.server.scheduler.runTaskTimer(plugin, Runnable {
+        syncTask = plugin.server.globalRegionScheduler.runAtFixedRate(plugin, { _ ->
             syncTicks += 10L
             val doObserverHUD = syncTicks % 20L == 0L
             
