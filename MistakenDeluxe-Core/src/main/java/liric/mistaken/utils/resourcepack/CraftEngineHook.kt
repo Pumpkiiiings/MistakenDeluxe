d973b7ab6510a56a5d998e4f55dca36f20647d57
Pumpkiiiings <pumpkiiiings@gmail.com>
2026-09-22 07:52:25 -0600

    Fix: Removed printStackTrace from CraftEngineHook to avoid console spam

--- MistakenDeluxe-Core/src/main/java/liric/mistaken/utils/resourcepack/CraftEngineHook.kt
+++ MistakenDeluxe-Core/src/main/java/liric/mistaken/utils/resourcepack/CraftEngineHook.kt
@@ -1,4 +1,4 @@
-﻿package liric.mistaken.utils.resourcepack
+package liric.mistaken.utils.resourcepack
 
 import net.momirealms.craftengine.bukkit.api.CraftEngineItems
 import net.momirealms.craftengine.core.util.Key
@@ -13,8 +13,7 @@ class CraftEngineHook : CustomItemProvider {
                 return itemDef.buildBukkitItem()
             }
         } catch (e: Exception) {
-            Mistaken.instance.logger.warning("Critical failure when requesting item from CraftEngine: $property")
-            e.printStackTrace()
+            Mistaken.instance.logger.warning("CraftEngine is disabled or failed to load. Skipping item: $property")
         }
         return null
     }
